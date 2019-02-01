4f507d3a8d70e96412a51260ad38fb61ceddbda5
nelong <nelong@gmail.com>
2019-02-01 14:00:41 -0600

    formatted 12.3 in sublime text

--- Windows/TestingExample/build_test_latex.bat
+++ Windows/TestingExample/build_test_latex.bat
@@ -1,13 +1,13 @@
 
 
 :: This is the output folder for the AC3vector for html
- set "OUTPUTFOLDER=C:\Users\longne\Documents\GitHub\active-calculus-vector\Windows\TestingExample\output"
+ set "OUTPUTFOLDER=C:\active-calculus-vector\Windows\TestingExample\output"
  
 :: This is the directory of the mathbookxml repository
- set "MBHTML=C:\Users\longne\Documents\GitHub\mathbook\xsl\mathbook-latex.xsl"
+ set "MBHTML=C:\mathbook\xsl\mathbook-latex.xsl"
 
  :: This is the Source file (.ptx)
-set "SRC=C:\Users\longne\Documents\GitHub\active-calculus-vector\Windows\TestingExample\quickstart.xml"
+set "SRC=C:\active-calculus-vector\Windows\TestingExample\quickstart.xml"
 
 :: This should make the html version of the project
 cd %OUTPUTFOLDER%
